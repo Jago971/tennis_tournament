0fb4387c93d4e113a28be1c38891fc718d031760
Matt_Mannings <matt.mannings97@gmail.com>
2024-09-16 16:57:26 +0100

    second commit

--- tennis_tournament.php
+++ tennis_tournament.php
@@ -12,10 +12,24 @@
 //To do list:
 
 //DB of players and info XXX
-//connect DB XX
+//connect DB XXX
+//extract player data as array XXX
 
 
-
+//----------------------------------------------------------------------------------------------------------------------DECLARE PLAYERS ARRAY
+$players = [];
+$surfaces = [
+    'lawn',
+    'clay',
+    'hard'
+];
+$weather = [
+    'rain',
+    'dry',
+    'snow',
+    'sunny'
+];
+//----------------------------------------------------------------------------------------------------------------------CONNECTING TO DB
 $db = new PDO(
     'mysql:host=DB;dbname=tennis_tournament',
     'root',
@@ -23,3 +37,46 @@ $db = new PDO(
 );
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT * FROM `players`');
+
+$result = $query->execute();
+
+if ($result) {
+    $players = $query->fetchAll();//------------------------------------------------------------------------------------REASSIGN PLAYERS ARRAY
+} else {
+    echo 'not working';
+}
+
+//Tournament function:
+
+//take in players
+//take two random players
+//decide winner
+//move winner to winners, move loser to losers
+//repeat until players is empty
+//reassign players as winners
+//repeat
+
+function playerScore($playerArr, $surface, $weather) {
+    $score = $playerArr[$surface] * $playerArr[$weather];
+}
+function randSurf(&$arr) {
+    shuffle($arr);
+    return array_pop($arr);
+}
+function game($arr)
+{
+    shuffle($arr);
+    $player1 = array_pop($arr);
+    $player2 = array_pop($arr);
+
+    var_dump($player1);
+    echo '<br>';
+    var_dump($player2);
+}
+var_dump($players);
+echo '<br>';
+game($players);
+echo '<br>';
+var_dump($players);
